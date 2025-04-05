278c0ee872ad0995d34577c5261e4b601b2e53c1
Kyle Pawlowski <kylepawlowski1@gmail.com>
2025-04-05 10:54:22 -0400

    add more hyperparameters to models and organize better

--- python/model_selection.py
+++ python/model_selection.py
@@ -6,14 +6,38 @@ Created on Mon Mar 17 17:15:09 2025
 @author: kyle
 """
 
-from models import Mars, ParameterModel
+from models import Mars, ParameterModel, DNN, RNNParam
 import numpy as np
-
-models = [Mars()]
+from itertools import chain
 
 datafile = ''
-
 x, y = ParameterModel.parse_data(datafile)
+n = x.shape[0]
+input_d = x.shape[1]
+output_d = y.shape[1]
+quarter_d = int(input_d/4)
+eighth_d = int(input_d/8)
+sixteenth_d = int(input_d/16)
+thirtysec_d = int(input_d/32)
+
+def Mars_model_gen():
+    for max_degree in range(2, 6):
+        for max_terms in np.logspace(2, 4, 3):
+            yield Mars(max_terms=max_terms, max_degree=max_degree)
+
+def DNN_model_gen():
+    for lr in np.logspace(-3, -1, 3):
+        yield DNN(output_d, (quarter_d, eighth_d, sixteenth_d, thirtysec_d), lr)
+        yield DNN(output_d, (sixteenth_d, thirtysec_d), lr)
+        
+def RNN_model_gen():
+    for lr in np.logspace(-3, -1, 3):
+        yield RNNParam(output_d, (quarter_d, eighth_d, sixteenth_d, thirtysec_d), input_d)
+        yield RNNParam(output_d, (sixteenth_d, thirtysec_d), input_d)
+        
+
+models = chain(Mars_model_gen(), DNN_model_gen(), RNN_model_gen())                      
+
 frac_train = 0.8
 num_train = int(len(x) * 0.8)
 num_val = len(x) - num_train
